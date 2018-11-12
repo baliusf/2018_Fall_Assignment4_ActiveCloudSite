afde48b831bbc99d5be30ce116c01a9498483026
Baranidhar E M <barani235@gmail.com>
2018-11-11 19:12:11 -0500

    CompaniesQuote.cs and Quote.cs added

---
+++ IEXTrading/Models/ViewModel/CompaniesQuote.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace IEXTrading.Models.ViewModel
+{
+    public class CompaniesQuoteRoot {
+        public Dictionary<string, Dictionary<string, Quote>> companiesQuoteRoot { get; set; }
+        public List<Company> Companies { get; set; }
+
+        public CompaniesQuoteRoot(List<Company> companies, Dictionary<string, Dictionary<string, Quote>> quoteRoot) {
+            Companies = companies;
+            companiesQuoteRoot = quoteRoot;
+        }
+    }
+
+    public class CompaniesQuote
+    {
+        public Dictionary<string, Quote> quote { get; set; }
+    }
+
+}
